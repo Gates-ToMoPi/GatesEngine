478d377cccd312ab7f4f1212bece4703f667a8b7
Gates-ToMoPi <67899020+Gates-ToMoPi@users.noreply.github.com>
2020-07-07 12:01:56 +0900

    CreateTriangleを削除

--- GatesEngine制作/GatesEngine制作/Dx12_ObjectFactory.h
+++ GatesEngine制作/GatesEngine制作/Dx12_ObjectFactory.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<d3d12.h>
+#include<vector>
 //いろいろな図形の頂点やらインデックスやら行列やらを生成して
 //引数で渡したオブジェクトに参照して書き換える
 class Dx12_ObjectFactory
@@ -7,10 +8,9 @@ class Dx12_ObjectFactory
 private:
 	ID3D12Device** pDevice;
 public:
-	Dx12_ObjectFactory(ID3D12Device**,ID3D12GraphicsCommandList**);
+	Dx12_ObjectFactory(ID3D12Device**);
 	~Dx12_ObjectFactory();
 	//2Dor3D
-	bool CreateTriangle();
 	bool CreateBox();
 	bool CreateCircle();
 	//3D
